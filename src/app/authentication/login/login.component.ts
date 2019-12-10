5600c4957fabe938e87ee7b5f02edc1bae1158aa
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-12-10 12:29:19 +0200

    added auth service

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -2,12 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import * as firebase from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {map} from 'rxjs/operators';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {WindowService} from '../../admin/shared/window.service';
+import {WindowService} from '../shared/window.service';
 import {User} from 'firebase';
 import {MatDialog, MatSnackBar} from '@angular/material';
-import {Router} from '@angular/router';
 import {AuthenticationService} from '../shared/authentication.service';
 
 @Component({
@@ -17,24 +15,25 @@ import {AuthenticationService} from '../shared/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  countryCode = '+380';
+  countryCode: string;
   phone: string;
   windowRef: any;
   verificationCode: string;
   user: any;
-  loginState: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   recaptchaVerifier: firebase.auth.RecaptchaVerifier;
-  smsSent: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  smsSent: BehaviorSubject<boolean>;
   sendSmsButtonDisable: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   constructor(private afAuth: AngularFireAuth,
               private db: AngularFirestore,
               private ws: WindowService,
-              private as: AuthenticationService,
+              private authenticationService: AuthenticationService,
               public dialog: MatDialog,
+              // private router: Router,
               // tslint:disable-next-line:variable-name
-              private _snackBar: MatSnackBar,
-              private router: Router) {
+              private _snackBar: MatSnackBar) {
+    this.countryCode = authenticationService.countryCode;
+    this.smsSent = authenticationService.smsSent;
   }
 
   ngOnInit() {
@@ -50,64 +49,69 @@ export class LoginComponent implements OnInit {
       }
     );
     this.recaptchaVerifier = this.windowRef.recaptchaVerifier;
-    this.windowRef.recaptchaVerifier.render();
+    this.windowRef.recaptchaVerifier.render().catch(console.log);
   }
 
-  getE164() {
-    const num = this.countryCode + this.phone;
-    return `${num}`;
-  }
+  // private getE164() {
+  //   const num = this.countryCode + this.phone;
+  //   return `${num}`;
+  // }
 
-  sendLoginCode() {
-
-    // const appVerifier = this.windowRef.recaptchaVerifier;
-
-    this.checkPhone().subscribe(value => {
-      if (value) {
-        this.loginState.next(true);
-        firebase.auth().signInWithPhoneNumber(this.getE164(), this.recaptchaVerifier)
-          .then(result => {
-            this.smsSent.next(true);
-            this.windowRef.confirmationResult = result;
-            // this.closeDialog();
-            // this.captchaState.next(true);
-          })
-          .catch(error => console.log(error + ' catch'));
-      } else {
-        this.onIncorrectPhone();
-      }
-    });
-  }
+  // sendLoginCode() {
+  //
+  //   // const appVerifier = this.windowRef.recaptchaVerifier;
+  //
+  //   this.checkPhone().subscribe(value => {
+  //     if (value) {
+  //       firebase.auth().signInWithPhoneNumber(this.getE164(), this.recaptchaVerifier)
+  //         .then(result => {
+  //           this.smsSent.next(true);
+  //           this.windowRef.confirmationResult = result;
+  //           // this.closeDialog();
+  //           // this.captchaState.next(true);
+  //         })
+  //         .catch(error => console.log(error + ' catch'));
+  //     } else {
+  //       this.onIncorrectPhone();
+  //     }
+  //   });
+  // }
 
   verifyLoginCode() {
-    this.windowRef.confirmationResult
-      .confirm(this.verificationCode)
-      .then(result => {
-
-        this.user = result.user;
-
-      })
-      .then(() => this.router.navigate(['']))
-      .catch(error => console.log(error, 'Incorrect code entered?'));
+    this.authenticationService.verifyLoginCode(this.verificationCode,  this.windowRef, this.user);
+    // this.windowRef.confirmationResult
+    //   .confirm(this.verificationCode)
+    //   .then(result => {
+    //
+    //     this.user = result.user;
+    //
+    //   })
+    //   .then(() => this.router.navigate(['']))
+    //   .catch(error => console.log(error, 'Incorrect code entered?'));
   }
 
-  off() {
-    this.afAuth.auth.signOut().catch(console.log).finally(() => this.loginState.next(false));
+  login() {
+    this.authenticationService.login(this.phone, this.recaptchaVerifier, this.windowRef);
   }
 
-  checkPhone(): Observable<boolean> {
-    return this.db
-      .collection('phones', ref => ref.where('phone', '==', Number.parseInt(this.phone, 10)))
-      .get()
-      .pipe(map(data => !data.empty));
+  logout() {
+    this.authenticationService.logout();
   }
+
+  // checkPhone(): Observable<boolean> {
+  //   return this.db
+  //     .collection('phones', ref => ref.where('phone', '==', Number.parseInt(this.phone, 10)))
+  //     .get()
+  //     .pipe(map(data => !data.empty));
+  // }
 
   isUser(): Observable<User> {
-    return this.afAuth.user;
+    // return this.afAuth.user;
+    return this.authenticationService.isUser();
   }
 
-  onIncorrectPhone() {
-    this._snackBar.open('Номер невірний', 'OK');
-  }
+  // onIncorrectPhone() {
+  //   this._snackBar.open('Номер невірний', 'OK');
+  // }
 }
 
--- src/app/authentication/shared/authentication.service.ts
+++ src/app/authentication/shared/authentication.service.ts
@@ -1,22 +1,89 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
+import {User} from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  smsSent: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private authState: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth,
+              private db: AngularFirestore,
+              // tslint:disable-next-line:variable-name
+              private _snackBar: MatSnackBar,
+              private router: Router) {
+    this.afAuth.authState.subscribe(user => this.authState = user);
+  }
+
+  // tslint:disable-next-line:variable-name
+  private _countryCode = '+380';
+
+  get countryCode(): string {
+    return this._countryCode;
+  }
 
   get authenticated(): boolean {
     return this.authState !== null;
   }
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(user => this.authState = user);
-  }
-
   logout(): void {
     this.afAuth.auth.signOut().catch(console.log);
+  }
+
+  verifyLoginCode(verificationCode: string, wr: any, user: any) {
+    wr.confirmationResult
+      .confirm(verificationCode)
+      .then(result => {
+
+        user = result.user;
+
+      })
+      .then(() => this.router.navigate(['']))
+      .catch(error => console.log(error, 'Incorrect code entered?'));
+  }
+
+  login(phone: string, recaptchaVerifier: firebase.auth.RecaptchaVerifier, wr: any) {
+    this.checkPhone(phone).subscribe(value => {
+      if (value) {
+        firebase.auth().signInWithPhoneNumber(this.getE164(phone), recaptchaVerifier)
+          .then(result => {
+            this.smsSent.next(true);
+            wr.confirmationResult = result;
+            // this.closeDialog();
+            // this.captchaState.next(true);
+          })
+          .catch(error => console.log(error + ' catch'));
+      } else {
+        this.onIncorrectPhone();
+      }
+    });
+  }
+
+  onIncorrectPhone() {
+    this._snackBar.open('Номер невірний', 'OK');
+  }
+
+  isUser(): Observable<User> {
+    return this.afAuth.user;
+  }
+
+  private getE164(phone: string) {
+    const num = this._countryCode + phone;
+    return `${num}`;
+  }
+
+  private checkPhone(phone: string): Observable<boolean> {
+    return this.db
+      .collection('phones', ref => ref.where('phone', '==', Number.parseInt(phone, 10)))
+      .get()
+      .pipe(map(data => !data.empty));
   }
 }
